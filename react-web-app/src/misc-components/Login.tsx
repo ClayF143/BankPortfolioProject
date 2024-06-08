75f546a01343ee1e5c1cb38afb6fbea2464c2145
ClayF143 <48032691+ClayF143@users.noreply.github.com>
2024-06-08 12:57:46 -0400

    google's image links sometimes don't work without a no-referrer policy, needed to use a different avatar component

--- react-web-app/src/misc-components/Login.tsx
+++ react-web-app/src/misc-components/Login.tsx
@@ -1,6 +1,7 @@
-import { Avatar, Tooltip } from "antd";
-import { UserOutlined } from '@ant-design/icons';
+import { Tooltip } from "antd";
 import { useAuth } from "../MyAuthProvider";
+import { useEffect } from "react";
+import { Avatar } from "@mui/material";
 
 function Login() {
   const { isAuthenticated, authUser, loginWithRedirect, logout } = useAuth();
@@ -8,15 +9,18 @@ function Login() {
     isAuthenticated ? logout() : loginWithRedirect();
   };
 
+  useEffect(() => {
+    console.log(authUser?.picture)
+  }, [authUser?.picture])
+
   return (
     <Tooltip placement="bottom" title={isAuthenticated ? "Logout" : "Login"} color="blue">
       <Avatar
         className="avatar-hover-effect"
         src={authUser?.picture}
         onClick={handleProfileClick}
-      >
-        {isAuthenticated ? <></> : <UserOutlined />}
-      </Avatar>
+        slotProps={{ img: { referrerPolicy: 'no-referrer' } }}
+      />
     </Tooltip>
   );
 }
